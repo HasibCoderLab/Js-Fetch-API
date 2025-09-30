7bd3f60b863e739572f658d65156762d400b7a49
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-30 12:32:54 +0600

    update Repo

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const URL = "https://catfact.ninja/fact";
-const para = document.querySelector("#fact")
-const btn = document.querySelector("button")
+const para = document.querySelector("#fact");
+const btn = document.querySelector("button");
 
 const getFact = async () =>{
     console.log("Geting ......");
@@ -8,14 +8,20 @@ const getFact = async () =>{
 let responce =  await fetch(URL);
     console.log(responce.status);
     console.log(responce);
-    responce.json;
-    let data = await responce.json()
+    // responce.json;
+    let data = await responce.json();
     
     console.log(data[1]);
-    para.innerText = data.fact
+    para.innerText = data.fact;
     
 
 
+}
+////// Fresh code 
+const getFact1  = async  () =>{
+let responce = await fetch(URL);
+let data = await responce.json();
+para.innerText = data.fact;
 }
 
 
@@ -25,9 +31,11 @@ const  getFacts = () =>{
         return res.json();
     }).then((data) =>{
         console.log(data);
-        para.innerText = data.fact
+        para.innerText = data.fact;
     
 
-    })
+    });
 }
-btn.addEventListener("click" , getFacts)
+btn.addEventListener("click" , getFacts);
+btn.addEventListener("click" , getFact);
+btn.addEventListener("click" , getFact1);
